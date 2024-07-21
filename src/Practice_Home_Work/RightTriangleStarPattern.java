c3a6728cebf82e8891cf987a1e906aba602327c5
Amit19012014 <Amit19012014@gmail.com>
2024-07-21 23:24:52 +0530

    21 July 2024 Home Work

--- src/Practice_Home_Work/RightTriangleStarPattern.java
+++ src/Practice_Home_Work/RightTriangleStarPattern.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RightTriangleStarPattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Number of max stars");
+        System.out.println("Enter the Number of stars");
 
         int n=sc.nextInt();
 
